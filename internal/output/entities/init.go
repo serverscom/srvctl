16310ccab2a4155c5b324775af862be50318bf14
Oleg Isakov <ion@servers.com>
2025-01-29 14:44:26 +0200

    rm excess comment

--- internal/output/entities/init.go
+++ internal/output/entities/init.go
@@ -1,16 +1,6 @@
 package entities
 
 // init registers all supported entities
-// to use custom header in table output:
-//
-//	Entity{
-//		Fields: []Field{
-//			{Name: "Name"},
-//			{Name: "Fingerprint", Header: "CUSTOM FINGERPRINT"},
-//			{Name: "Created"},
-//			{Name: "Updated", Header: "UPDATED_AT"},
-//		},
-//	}
 func init() {
 	RegisterSSHKeyDefinition()
 }
