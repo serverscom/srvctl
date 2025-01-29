3c0fdfd91ccb1a596aefeaa1e9cbb4e7e817bb7d
Oleg Isakov <ion@servers.com>
2025-01-29 15:18:43 +0200

    use title for fields paths

--- internal/output/entities/ssh_keys.go
+++ internal/output/entities/ssh_keys.go
@@ -14,11 +14,11 @@ var (
 func RegisterSSHKeyDefinition() {
 	sshEntity := &Entity{
 		fields: []Field{
-			{ID: "Name", Name: "Name", Path: "name", ListHandlerFunc: stringHandler, PageViewHandlerFunc: stringHandler, Default: true},
-			{ID: "Fingerprint", Name: "Fingerprint", Path: "fingerprint", ListHandlerFunc: stringHandler, PageViewHandlerFunc: stringHandler, Default: true},
-			{ID: "Labels", Name: "Labels", Path: "labels", ListHandlerFunc: mapListHandler, PageViewHandlerFunc: mapPageHandler},
-			{ID: "Created", Name: "Created", Path: "created", ListHandlerFunc: timeHandler, PageViewHandlerFunc: timeHandler, Default: true},
-			{ID: "Updated", Name: "Updated", Path: "updated", ListHandlerFunc: timeHandler, PageViewHandlerFunc: timeHandler, Default: true},
+			{ID: "Name", Name: "Name", Path: "Name", ListHandlerFunc: stringHandler, PageViewHandlerFunc: stringHandler, Default: true},
+			{ID: "Fingerprint", Name: "Fingerprint", Path: "Fingerprint", ListHandlerFunc: stringHandler, PageViewHandlerFunc: stringHandler, Default: true},
+			{ID: "Labels", Name: "Labels", Path: "Labels", ListHandlerFunc: mapListHandler, PageViewHandlerFunc: mapPageHandler},
+			{ID: "Created", Name: "Created", Path: "Created", ListHandlerFunc: timeHandler, PageViewHandlerFunc: timeHandler, Default: true},
+			{ID: "Updated", Name: "Updated", Path: "Updated", ListHandlerFunc: timeHandler, PageViewHandlerFunc: timeHandler, Default: true},
 		},
 		eType: SSHKeyType,
 	}
