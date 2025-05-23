60682723c710c93a4a868cf337772063db32741f
Oleg Isakov <ion@servers.com>
2025-05-23 15:11:17 +0300

    use separate add commands for different host types; add create flags for ds

--- cmd/entities/hosts/add.go
+++ cmd/entities/hosts/add.go
@@ -1,53 +1,203 @@
 package hosts
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	serverscom "github.com/serverscom/serverscom-go-client/pkg"
 	"github.com/serverscom/srvctl/cmd/base"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
-type HostCreator interface {
-	Create(ctx context.Context, client *serverscom.Client, input any) (any, error)
-	NewCreateInput() any
+type AddDSFlags struct {
+	InputPath         string
+	LocationID        int
+	ServerModelID     int
+	OperatingSystemID int
+	Features          []string
+	RAMSize           int
+	PublicUplinkID    int
+	PublicBandwidthID int
+	PrivateUplinkID   int
+	DriveSlots        map[string]int
+	Layout            string
+	Partitions        []string
+	IPv6              bool
+	UserDataFile      string
+	UserData          string
+	Labels            map[string]string
 }
 
-type DSCreateMgr struct{}
-
-func (c *DSCreateMgr) Create(ctx context.Context, client *serverscom.Client, input any) (any, error) {
-	dsInput, ok := input.(*serverscom.DedicatedServerCreateInput)
-	if !ok {
-		return nil, fmt.Errorf("invalid input type for dedicated server")
+func applyFlagsToInput(
+	input *serverscom.DedicatedServerCreateInput,
+	flags *AddDSFlags,
+	pflags *pflag.FlagSet,
+) error {
+	if pflags.Changed("location-id") {
+		input.LocationID = int64(flags.LocationID)
 	}
-	return client.Hosts.CreateDedicatedServers(ctx, *dsInput)
-}
-
-func (c *DSCreateMgr) NewCreateInput() any {
-	return &serverscom.DedicatedServerCreateInput{}
-}
-
-type SBMCreateMgr struct{}
-
-func (c *SBMCreateMgr) Create(ctx context.Context, client *serverscom.Client, input any) (any, error) {
-	sbmInput, ok := input.(*serverscom.SBMServerCreateInput)
-	if !ok {
-		return nil, fmt.Errorf("invalid input type for SBM server")
+	if pflags.Changed("server-model-id") {
+		input.ServerModelID = int64(flags.ServerModelID)
 	}
-	return client.Hosts.CreateSBMServers(ctx, *sbmInput)
+	if pflags.Changed("operating-system-id") {
+		val := int64(flags.OperatingSystemID)
+		input.OperatingSystemID = &val
+	}
+	if pflags.Changed("feature") {
+		input.Features = flags.Features
+	}
+	if pflags.Changed("ram-size") {
+		input.RAMSize = flags.RAMSize
+	}
+	if pflags.Changed("public-uplink-id") {
+		if input.UplinkModels.Public == nil {
+			input.UplinkModels.Public = &serverscom.DedicatedServerPublicUplinkInput{}
+		}
+		input.UplinkModels.Public.ID = int64(flags.PublicUplinkID)
+	}
+	if pflags.Changed("public-bandwidth-id") {
+		if input.UplinkModels.Public == nil {
+			input.UplinkModels.Public = &serverscom.DedicatedServerPublicUplinkInput{}
+		}
+		input.UplinkModels.Public.BandwidthModelID = int64(flags.PublicBandwidthID)
+	}
+	if pflags.Changed("private-uplink-id") {
+		input.UplinkModels.Private.ID = int64(flags.PrivateUplinkID)
+	}
+
+	if pflags.Changed("drive-slots") {
+		slots, err := parseDriveSlots(flags.DriveSlots)
+		if err != nil {
+			return err
+		}
+		input.Drives.Slots = slots
+	}
+
+	if pflags.Changed("layout") {
+		layout, err := parseLayout(flags.Layout)
+		if err != nil {
+			return err
+		}
+		input.Drives.Layout = layout
+	}
+
+	if pflags.Changed("partition") {
+		if len(input.Drives.Layout) == 0 {
+			return fmt.Errorf("partition given but layout is empty")
+		}
+		partitions, err := parsePartitions(flags.Partitions)
+		if err != nil {
+			return err
+		}
+		input.Drives.Layout[0].Partitions = partitions
+	}
+	if pflags.Changed("ipv6") {
+		input.IPv6 = flags.IPv6
+	}
+	if pflags.Changed("user-data") && pflags.Changed("user-data-file") {
+		return fmt.Errorf("'user-data' and 'user-data-file' can't be used together")
+	}
+	if pflags.Changed("user-data-file") {
+		data, err := os.ReadFile(flags.UserDataFile)
+		if err != nil {
+			return fmt.Errorf("can't read user-data-file: %v", err)
+		}
+		dataStr := string(data)
+		input.UserData = &dataStr
+	}
+	if pflags.Changed("user-data") {
+		input.UserData = &flags.UserData
+	}
+
+	if pflags.Changed("labels") {
+		for i := range input.Hosts {
+			input.Hosts[i].Labels = flags.Labels
+		}
+	}
+	return nil
 }
 
-func (c *SBMCreateMgr) NewCreateInput() any {
-	return &serverscom.SBMServerCreateInput{}
+func newAddDSCmd(cmdContext *base.CmdContext) *cobra.Command {
+	flags := &AddDSFlags{}
+
+	cmd := &cobra.Command{
+		Use:   "add",
+		Short: "Create a dedicated server",
+		Args:  cobra.ArbitraryArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			manager := cmdContext.GetManager()
+			ctx, cancel := base.SetupContext(cmd, manager)
+			defer cancel()
+
+			base.SetupProxy(cmd, manager)
+
+			input := serverscom.DedicatedServerCreateInput{}
+
+			if err := base.ReadInputJSON(flags.InputPath, cmd.InOrStdin(), &input); err != nil {
+				return err
+			}
+			if len(input.Hosts) == 0 && len(args) == 0 {
+				return fmt.Errorf("no hosts found from positional args and no hosts found from input, can't continue")
+			}
+
+			for _, hostname := range args {
+				input.Hosts = append(input.Hosts, serverscom.DedicatedServerHostInput{
+					Hostname: hostname,
+				})
+			}
+
+			err := applyFlagsToInput(&input, flags, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			scClient := cmdContext.GetClient().SetVerbose(manager.GetVerbose(cmd)).GetScClient()
+
+			server, err := scClient.Hosts.CreateDedicatedServers(ctx, input)
+			if err != nil {
+				return err
+			}
+
+			if server != nil {
+				formatter := cmdContext.GetOrCreateFormatter(cmd)
+				return formatter.Format(server)
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&flags.InputPath, "input", "i", "", "path to input file or '-' to read from stdin")
+
+	cmd.Flags().IntVar(&flags.LocationID, "location-id", 0, "Create the server(s) in the specific location ID")
+	cmd.Flags().IntVar(&flags.ServerModelID, "server-model-id", 0, "Use specific server model ID to create the server")
+	cmd.Flags().IntVar(&flags.OperatingSystemID, "operating-system-id", 0, "Install the specific operating system")
+	cmd.Flags().StringSliceVar(&flags.Features, "feature", nil, "Set of features")
+	cmd.Flags().IntVar(&flags.RAMSize, "ram-size", 0, "Desired amount of RAM in GB")
+	cmd.Flags().IntVar(&flags.PublicUplinkID, "public-uplink-id", 0, "The public uplink ID, can be omitted if do not want public uplink")
+	cmd.Flags().IntVar(&flags.PrivateUplinkID, "private-uplink-id", 0, "The private uplink ID")
+	cmd.Flags().IntVar(&flags.PublicBandwidthID, "public-bandwidth-id", 0, "The public bandwidth ID, MUST be omitted if public uplink id is not passed")
+	cmd.Flags().StringToIntVar(&flags.DriveSlots, "drive-slots", nil, "mapping of the specific slot to the specific drive model")
+	cmd.Flags().StringVar(&flags.Layout, "layout", "", "Configuration of drives layout")
+	cmd.Flags().StringArrayVar(&flags.Partitions, "partition", nil, "Configuration of the specific partitions")
+	cmd.Flags().BoolVar(&flags.IPv6, "ipv6", false, "Enable IPv6")
+	cmd.Flags().StringVar(&flags.UserDataFile, "user-data-file", "", "Path to user data which should be readed")
+	cmd.Flags().StringVar(&flags.UserData, "user-data", "", "Content of user data")
+	cmd.Flags().StringToStringVar(&flags.Labels, "labels", nil, "The set of labels which will be applied to the all hosts of this operation")
+
+	return cmd
 }
 
-func newAddCmd(cmdContext *base.CmdContext, hostType *HostTypeCmd) *cobra.Command {
+func newAddSBMCmd(cmdContext *base.CmdContext) *cobra.Command {
 	var path string
 	cmd := &cobra.Command{
 		Use:   "add --input <path>",
-		Short: fmt.Sprintf("Create a %s", hostType.entityName),
+		Short: "Create an SBM server",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			manager := cmdContext.GetManager()
@@ -56,15 +206,15 @@ func newAddCmd(cmdContext *base.CmdContext, hostType *HostTypeCmd) *cobra.Comman
 
 			base.SetupProxy(cmd, manager)
 
-			input := hostType.managers.createMgr.NewCreateInput()
+			input := serverscom.SBMServerCreateInput{}
 
-			if err := base.ReadInputJSON(path, cmd.InOrStdin(), input); err != nil {
+			if err := base.ReadInputJSON(path, cmd.InOrStdin(), &input); err != nil {
 				return err
 			}
 
 			scClient := cmdContext.GetClient().SetVerbose(manager.GetVerbose(cmd)).GetScClient()
 
-			server, err := hostType.managers.createMgr.Create(ctx, scClient, input)
+			server, err := scClient.Hosts.CreateSBMServers(ctx, input)
 			if err != nil {
 				return err
 			}
@@ -84,4 +234,84 @@ func newAddCmd(cmdContext *base.CmdContext, hostType *HostTypeCmd) *cobra.Comman
 	}
 
 	return cmd
+}
+
+func parseDriveSlots(driveSlots map[string]int) ([]serverscom.DedicatedServerSlotInput, error) {
+	slots := make([]serverscom.DedicatedServerSlotInput, 0, len(driveSlots))
+	for pos, id := range driveSlots {
+		dId := int64(id)
+		posNum, err := strconv.Atoi(pos)
+		if err != nil {
+			return nil, fmt.Errorf("can't parse drive slot position '%s' as integer", pos)
+		}
+		slots = append(slots, serverscom.DedicatedServerSlotInput{
+			Position:     posNum,
+			DriveModelID: &dId,
+		})
+	}
+	return slots, nil
+}
+
+func parseLayout(s string) ([]serverscom.DedicatedServerLayoutInput, error) {
+	parts := strings.Split(s, ",")
+	var layout serverscom.DedicatedServerLayoutInput
+
+	for _, part := range parts {
+		pair := strings.SplitN(part, "=", 2)
+		if len(pair) != 2 {
+			continue
+		}
+		key := pair[0]
+		val := pair[1]
+
+		switch key {
+		case "slot":
+			num, err := strconv.Atoi(val)
+			if err != nil {
+				return nil, fmt.Errorf("can't parse layout slot '%s' as integer", val)
+			}
+			layout.SlotPositions = append(layout.SlotPositions, num)
+		case "raid":
+			raid, err := strconv.Atoi(val)
+			if err != nil {
+				return nil, fmt.Errorf("can't parse layout raid '%s' as integer", val)
+			}
+			layout.Raid = &raid
+		}
+	}
+	return []serverscom.DedicatedServerLayoutInput{layout}, nil
+}
+
+func parsePartitions(values []string) ([]serverscom.DedicatedServerLayoutPartitionInput, error) {
+	var result []serverscom.DedicatedServerLayoutPartitionInput
+
+	for _, s := range values {
+		var p serverscom.DedicatedServerLayoutPartitionInput
+		parts := strings.Split(s, ",")
+		for _, part := range parts {
+			pair := strings.SplitN(part, "=", 2)
+			if len(pair) != 2 {
+				continue
+			}
+			key, val := pair[0], pair[1]
+
+			switch key {
+			case "target":
+				p.Target = val
+			case "size":
+				size, err := strconv.Atoi(val)
+				if err != nil {
+					return nil, fmt.Errorf("can't parse partition size '%s' as integer", val)
+				}
+				p.Size = size
+			case "fs":
+				p.Fs = &val
+			case "fill":
+				p.Fill = (val == "true")
+			}
+		}
+		result = append(result, p)
+	}
+
+	return result, nil
 }
